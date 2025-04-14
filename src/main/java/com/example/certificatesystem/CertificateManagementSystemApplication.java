6124c0c95eabe15dc62e91126b85ccaa7f80a52a
iammohammad <mohammad73.ah@gmail.com>
2025-04-15 02:15:32 +0330

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/certificatesystem/CertificateManagementSystemApplication.java
@@ -0,0 +1,11 @@
+package com.example.certificatesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CertificateManagementSystemApplication.class, args);
+    }
+}
