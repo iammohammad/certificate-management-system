c64e08430dbae898579ef32516366af407a89965
iammohammad <mohammad73.ah@gmail.com>
2025-04-15 19:00:55 +0330

    Replace project with new Spring Boot setup

--- certificate-management-system/src/main/java/com/mohammad/certificatemanagementsystem/CertificateManagementSystemApplication.java
+++ certificate-management-system/src/main/java/com/mohammad/certificatemanagementsystem/CertificateManagementSystemApplication.java
@@ -1,11 +1,13 @@
-package com.example.certificatesystem;
+package com.mohammad.certificatemanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CertificateManagementSystemApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CertificateManagementSystemApplication.class, args);
     }
+
 }
