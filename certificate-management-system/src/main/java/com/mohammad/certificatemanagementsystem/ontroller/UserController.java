ad9b89766867a2bed6e2b93fa94ff92bfe187909
iammohammad <mohammad73.ah@gmail.com>
2025-04-21 20:34:38 +0330

    add UserController

---
+++ certificate-management-system/src/main/java/com/mohammad/certificatemanagementsystem/ontroller/UserController.java
@@ -0,0 +1,37 @@
+package com.mohammad.certificatemanagementsystem.ontroller;
+
+import com.mohammad.certificatemanagementsystem.entity.User;
+import com.mohammad.certificatemanagementsystem.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/adduser")
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+}
